5fc39112dd6bc12ec1706a705c9be4fddc734c42
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-29 10:49:12 -0700

    Add tests for plus_one

--- arrays/array_tests.py
+++ arrays/array_tests.py
@@ -20,5 +20,15 @@ class TestDutchFlagPartition(unittest.TestCase):
         greater_than = map(lambda x: x > self.pivot, right)
         self.assertTrue(all(greater_than))
 
+class TestPlusOne(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(plus_one([1,2,8]), [1,2,9])
+
+    def test_carry_the_one(self):
+        self.assertEqual(plus_one([1,2,9]), [1,3,0])
+
+    def test_add_digit(self):
+        self.assertEqual(plus_one([9,9]), [1,0,0])
+
 if __name__ == '__main__':
     unittest.main()

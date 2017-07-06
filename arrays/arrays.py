dd27add420c64e3bb58900c10b5fe1bae043aaf8
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-06 15:01:30 -0700

    Add is_valid_sudoku

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def even_odd(A):
     """given an array of integers, move all evens to the front. in place"""
@@ -68,3 +69,24 @@ def random_subset(A, k):
         A[i], A[r] = A[r], A[i]
 
     return A
+
+def is_valid_sudoku(partial_assignment):
+    def has_duplicate(block):
+        block = list(filter(lambda x: x != 0, block))
+        return len(block) != len(set(block))
+
+    n = len(partial_assignment)
+    # check rows and cols
+    if any(
+            has_duplicate([partial_assignment[i][j] for j in range(n)]) or
+            has_duplicate([partial_assignment[j][i] for j in range(n)])
+            for i in range(n)):
+        return False
+
+    # check regions
+    region_size = int(math.sqrt(n))
+    return all(not has_duplicate([
+        partial_assignment[a][b]
+        for a in range(region_size * I, region_size * (I + 1))
+        for b in range(region_size * J, region_size * (J + 1))
+    ]) for I in range(region_size) for J in range(region_size))

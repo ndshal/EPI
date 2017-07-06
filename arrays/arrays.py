8e0fc5137ea58f53fab3e3cfd9dc86b49956ed7e
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-06 14:29:15 -0700

    Implement buy_sell_stock_once

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -48,5 +48,12 @@ def plus_one(A):
 
     return A
 
+# given an array representing stock prices, find max profit
 def buy_sell_stock_once(prices):
-    return
+    min_price_so_far, max_profit = float('inf'), 0.0
+    for price in prices:
+        profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+
+    return max_profit

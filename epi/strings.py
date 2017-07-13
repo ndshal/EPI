fefe3612165781be6f9d8356ef91d1e221ee2f28
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-13 16:31:02 -0700

    Start implementing convert_base

--- epi/strings.py
+++ epi/strings.py
@@ -1,5 +1,6 @@
 import string
 from functools import reduce
+import math
 
 def int_to_string(x):
     """Convert int to string"""
@@ -22,6 +23,54 @@ def string_to_int(s):
         s[s[0] == '-':], 0
         )*(-1 if s[0] == '-' else 1)
 
+def convert_decimal_to_base(s, b):
+    num = int(s)
+    power = int(math.log(num, b))
+    digits = []
+
+    while num > 0:
+        digit = num // b**power
+        digits.append(str(digit))
+        num -= digit * b**power
+        power -= 1
+
+    return ''.join(reversed(digits))
+
 def convert_base(s, b1, b2):
     """Convert string representation of an integer from b1 to b2"""
-    return s
+    def convert_decimal_to_base(s, b):
+        num = int(s)
+        power = int(math.log(num, b))
+        digits = []
+
+        while power >= 0:
+            digit = num // b**power
+            digits.append(str(digit))
+            num -= digit * b**power
+            power -= 1
+
+        return ''.join(reversed(digits))
+
+    def convert_base_to_decimal(s, b):
+        dec = {
+            '0': 0,
+            '1': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            'A': 10,
+            'B': 11,
+            'C': 12,
+            'D': 13,
+            'E': 14,
+            'F': 15,
+        }
+
+        return str(reduce(lambda acc, el: acc + el, (dec[s[~i]] * b**i for i in range(len(s)))))
+
+    return convert_decimal_to_base(convert_base_to_decimal(s, b1), b2)

38f4b7f00ad12d3979d54bafbb8e56329fd70bf1
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-17 12:43:59 -0700

    Implement is_palindrome

--- epi/strings.py
+++ epi/strings.py
@@ -68,4 +68,16 @@ def replace_and_remove(size, s):
     return s
 
 def is_palindrome(s):
-    return False
+    """Determines if s is a palidrome, ignoring non alphanumeric chars"""
+    i, j = 0, len(s) - 1
+    while i < j:
+        # ignore non-alphanumeric chars
+        while not s[i].isalnum() and i < j:
+            i += 1
+        while not s[j].isalnum() and i < j:
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i, j = i + 1, j - 1
+
+    return True

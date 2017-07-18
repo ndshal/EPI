efa3cdd1be5f242cf87950cd30478021620f43ff
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-18 10:09:29 -0700

    Implement merge_two_sorted_klists

--- epi/linked_lists.py
+++ epi/linked_lists.py
@@ -18,3 +18,18 @@ def delete(node):
 
 def delete_after(node):
     node.next = node.next.next
+
+def merge_two_sorted_lists(L1, L2):
+    dummy_head = tail = ListNode()
+
+    while L1 and L2:
+        if L1.data < L2.data:
+            tail.next, L1 = L1, L1.next
+        else:
+            tail.next, L2 = L2, L2.next
+        tail = tail.next
+
+    # either L1 or L2 is None
+    tail.next = L1 or L2
+
+    return dummy_head.next

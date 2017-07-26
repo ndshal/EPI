035f99e824c3e49a01db2ea9f6b06b2aaf91ff4c
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-25 17:11:27 -0700

    Add iterative bsearch

--- epi/search.py
+++ epi/search.py
@@ -1,3 +1,17 @@
+def bsearch(A, t):
+    """Iterative binary search for t"""
+    left, right = 0, len(A) - 1
+    while left <= right:
+        mid = left + (right - left) // 2 #(L+U)/2 could cause overflow
+        if A[mid] < t:
+            left = mid + 1
+        elif A[mid] == t:
+            return mid
+        else:
+            right = mid - 1
+
+    return -1
+
 def search_first_of_k(A, k):
     """Return the index of the first occurence of k in A (or -1)"""
     return -1

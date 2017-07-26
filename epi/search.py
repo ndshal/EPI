428f01880061deb546b608971ae957cc802814e7
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-25 17:17:20 -0700

    Implement search_first_of_k

--- epi/search.py
+++ epi/search.py
@@ -14,4 +14,16 @@ def bsearch(A, t):
 
 def search_first_of_k(A, k):
     """Return the index of the first occurence of k in A (or -1)"""
-    return -1
+    # perform binary search. If found, continue searching left anyway
+    left, right, result = 0, len(A) - 1, -1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if A[mid] < k:
+            left = mid + 1
+        elif A[mid] == k:
+            result = mid
+            right = mid - 1
+        else:
+            right = mid - 1
+
+    return result
--- tests/search_test.py
+++ tests/search_test.py
@@ -4,7 +4,7 @@ from epi.search import *
 class TestFindFirstOccurence(unittest.TestCase):
     def test_search_first_of_k(self):
         self.assertEqual(search_first_of_k(
-            [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 103), 3)
+            [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 108), 3)
 
 if __name__ == '__main__':
     unittest.main()
